318cc42f32dab434834455524ba132605ab7d9b2
LEE <sw09112@yonsei.ac.kr>
2025-02-02 11:34:25 +0900

    first commit

---
+++ src/chap_01/_03_Variables.java
@@ -0,0 +1,34 @@
+package chap_01;
+
+public class _03_Variables {
+    public static void main(String[] args) {
+        String name;
+        name = "석원";
+
+        int time = 15;
+
+
+        System.out.println(name + "님, 배송이 시작됩니다." + time + "시에 방문 예정입니다.");
+        System.out.println(name + "님, 배송이 완료되었습니다.");
+
+        double score = 90.5;
+        char grade = 'A';
+
+        name = "웅";
+
+        System.out.println( name + "님의 점수는 " + score + "이고 따라서 학점은 " + grade + "입니다.");
+
+        boolean pass = true;
+        System.out.println("이번 시험은 " + pass);
+
+        double d = 3.14;
+        float f = 3.14f;
+        System.out.println(d);
+        System.out.println(f);
+
+        System.out.println(name + "이 지금 승주집 갈래?");
+
+
+
+    }
+}
